ccf51a86944c406538bb716329733fe425332170
Miguel <miguelfracassi@gmail.com>
2022-04-05 10:36:35 -0300

    aulaweb05/04

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+// auto load
+
+spl_autoload_extensions('.php');
+
+function classLoader($class)
+{
+    $pastas = array('controller', 'model');
+    foreach ($pastas as $pasta) {
+        $arquivo = "{$pasta}/{$class}.php";
+        if (file_exists($arquivo)) {
+            require_once($arquivo);
+        }
+    }
+}
+
+spl_autoload_register("classLoader");
+
+//Front controller
+
+class Aplicacao{
+    public static function run(){
+
+            $layout = new Template("view/layout.html");
+
+            $class="Inicio";
+            if (class_exists($class)){
+                $pagina = new $class();
+                $conteudo = $pagina -> controller();
+                $layout->set("conteudo", "$conteudo");
+            }
+
+            echo $layout -> saida();
+
+    }
+}
+
+Aplicacao::run();
+
+?>
